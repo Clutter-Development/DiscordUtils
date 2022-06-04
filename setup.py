274213d8124816e615fac0fd9d19c64f4ff946b4
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-04 19:41:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="discord_utils",
     author="Clutter Development",
     version="1.0.0",
     license="MIT",
     description="A collection of good and simple utilities for Discord bots.",
-    install_requires=requirements,
     python_requires=">=3.10",
     py_modules=["discord_utils"],
     packages=["discord_utils"],
